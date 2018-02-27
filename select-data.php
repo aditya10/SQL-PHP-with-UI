d994863c5b038ac0362f9fe8a294c7066d34f2de
Adi <aditya.chinchure@gmail.com>
2018-02-27 15:03:23 -0800

    Added Departures

--- select-data.php
+++ select-data.php
@@ -1,6 +1,7 @@
 <html>
 <head>
   <link rel="stylesheet" href="design.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 </head>
     <body>
       <div class="wrapper">
@@ -8,6 +9,7 @@
           <h2>Airport Database</h2>
         </div>
         <div class="table">
+          <h3>Arrivals</h3>
         <?php
         $servername = "localhost";
         $username = "root";
@@ -33,12 +35,46 @@
                 echo "<td>" . $row["flight_num"]. "</td><td>" . $row["depcity"]. "</td><td>" . $row["arrcity"]. "</td>";
                 echo "</tr>";
             }
+            echo "</table>";
         } else {
             echo "0 results";
         }
         $conn->close();
         ?>
       </div>
+      <div class="table">
+        <h3>Departures</h3>
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "AirportSample";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $now = date("Y-m-d H:i:s");
+        $sql = "SELECT flight_num, depcity, arrcity FROM Plane WHERE arrtime<'$now' AND deptime>'$now'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            echo "<table><tr><th>Flight Num</th><th>Departure City</th><th>Arrival City</th></tr>";
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row["flight_num"]. "</td><td>" . $row["depcity"]. "</td><td>" . $row["arrcity"]. "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+        ?>
     </div>
   </body>
 </html>
