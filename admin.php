2c1804d4a7ebf281ac483ac017b4c28e20fb24af
Adi <aditya.chinchure@gmail.com>
2018-03-28 17:41:56 -0700

    Admin can now add/update/delete/search airport staff

--- admin.php
+++ admin.php
@@ -1,30 +1,4 @@
-<?php
-include("connect.php");
-session_start();
-if(isset($_POST['submit_entry'])) {
-		// Escape user inputs for security
-		$license_num = mysqli_real_escape_string($conn, $_REQUEST['license_num']);
-		$position = mysqli_real_escape_string($conn, $_REQUEST['position']);
-		$staff_name = mysqli_real_escape_string($conn, $_REQUEST['staff_name']);
 
-	if($license_num>0){
-		// attempt insert query execution
-		$sql = "INSERT INTO airport_staff VALUES ('$license_num', '$position', '$staff_name')";
-		if(mysqli_query($conn, $sql)){
-			echo "Records added successfully.";
-		} else{
-			echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
-		}
-	} else{
-		echo "error";
-		echo $license_num;
-	}
-	
-		
-}
-// close connection
-mysqli_close($conn);
-?>
 
 <html lang="en">
 <head>
@@ -45,9 +19,100 @@ mysqli_close($conn);
         <label for="staff_name">Name:</label>
         <input type="text" name="staff_name" id="staff_name">
     </p>
-    <input type="submit" value="Submit" name="submit_entry">
+    <input type="submit" value="Add Person" name="submit_entry">
+	<input type="submit" value="Search" name="search_license">
+	<input type="submit" value="Delete" name="delete_license">
+	<input type="submit" value="Update" name="update_license">
 	
 </form>
+	
+	
+	<?php
+include("connect.php");
+session_start();
+if(isset($_POST['submit_entry'])) {
+		// Escape user inputs for security
+		$license_num = mysqli_real_escape_string($conn, $_REQUEST['license_num']);
+		$position = mysqli_real_escape_string($conn, $_REQUEST['position']);
+		$staff_name = mysqli_real_escape_string($conn, $_REQUEST['staff_name']);
+
+	if($license_num>0){
+		// attempt insert query execution
+		$sql = "INSERT INTO airport_staff VALUES ('$license_num', '$position', '$staff_name')";
+		if(mysqli_query($conn, $sql)){
+			echo "Records added successfully.";
+		} else{
+			echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+		}
+	} else{
+		echo "Error: invalid license number entered.";
+	}
+}
+
+if(isset($_POST['search_license'])) {
+		// Escape user inputs for security
+		$license_num = mysqli_real_escape_string($conn, $_REQUEST['license_num']);
+
+	$sql = "SELECT * FROM airport_staff WHERE license_num='$license_num'";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+		// output data of each row
+		echo "<table><tr><th>License No</th><th>Position</th><th>Name</th></tr>";
+		while($row = $result->fetch_assoc()) {
+			echo "<tr>";
+			echo "<td>" . $row["license_num"]. "</td><td>" . $row["position"]. "</td><td>" . $row["staff_name"]. "</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+	} else {
+		echo "0 results";
+	}
+}
+
+if(isset($_POST['delete_license'])) {
+		// Escape user inputs for security
+		$license_num = mysqli_real_escape_string($conn, $_REQUEST['license_num']);
+
+	if($license_num>0){
+		// attempt insert query execution
+		$sql = "DELETE FROM airport_staff WHERE license_num='$license_num'";
+		if(mysqli_query($conn, $sql)){
+			echo "Record deleted successfully.";
+		} else{
+			echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+		}
+	} else{
+		echo "Error: invalid license number entered.";
+	}
+}
+	
+if(isset($_POST['update_license'])) {
+		// Escape user inputs for security
+		$license_num = mysqli_real_escape_string($conn, $_REQUEST['license_num']);
+		$position = mysqli_real_escape_string($conn, $_REQUEST['position']);
+		$staff_name = mysqli_real_escape_string($conn, $_REQUEST['staff_name']);
+
+	if($license_num>0){
+		// attempt insert query execution
+		$sql = "UPDATE airport_staff SET position='$position', staff_name='$staff_name' WHERE license_num='$license_num'";
+		if(mysqli_query($conn, $sql)){
+			echo "Record updated successfully.";
+		} else{
+			echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+		}
+	} else{
+		echo "Error: invalid license number entered.";
+	}
+}
+
+
+
+// close connection
+mysqli_close($conn);
+?>
+	
+	
 	
 	<br>
 	<br>
@@ -72,5 +137,7 @@ mysqli_close($conn);
 	// close connection
 	mysqli_close($conn);
     ?>
+
+	
 </body>
 </html>
